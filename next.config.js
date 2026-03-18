16669ecac36df5834b10684dacf9c68d13d888c1
happy-code-vector <kevinallen5877066@gmail.com>
2026-03-18 09:58:54 +0300

    fix: seed script

--- next.config.js
+++ next.config.js
@@ -13,6 +13,18 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.cdnlogo.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+      },
     ],
   },
 };
